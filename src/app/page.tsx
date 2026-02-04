6054f78d6a36313c4e6533e30672a5bb063727e4
Gael Monts <gaelmontesluna@gmail.com>
2026-02-04 18:00:56 -0500

    feat: add WhatsApp button component for direct contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import ReviewsCarousel from "@/components/ReviewsCarousel";
 import LatestPosts from "@/components/LatestPosts";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import WhatsAppButton from "@/components/WhatsAppButton";
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
       <ReviewsCarousel />
       <LatestPosts />
       <Footer />
+      <WhatsAppButton />
     </div>
   );
 }
